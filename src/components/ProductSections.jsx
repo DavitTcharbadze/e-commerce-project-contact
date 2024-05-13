e65121f8f35c08770be16835058f26b605e63d24
DavitTcharbadze <153432509+DavitTcharbadze@users.noreply.github.com>
2024-05-13 19:30:07 +0400

    Update ProductSections.jsx

--- src/components/ProductSections.jsx
+++ src/components/ProductSections.jsx
@@ -17,15 +17,15 @@ const ProductSections = () => {
     return (
         <div className={classes['sections-wrapper']}>
             <div className={classes['sections']}>
-                <div className={classes['box']} onClick={() => handleSectionClick(1)}><h3>Woman's Fashion</h3></div>
-                <div className={classes['box']} onClick={() => handleSectionClick(2)}><h3>Man's Fashion</h3></div>
-                <div className={classes['box']} onClick={() => handleSectionClick(3)}><h3>Electronics</h3></div>
-                <div className={classes['box']} onClick={() => handleSectionClick(4)}><h3>Home & Lifestyle</h3></div>
-                <div className={classes['box']} onClick={() => handleSectionClick(5)}><h3>Medicine</h3></div>
-                <div className={classes['box']} onClick={() => handleSectionClick(6)}><h3>Sports & Outdoor</h3></div>
-                <div className={classes['box']} onClick={() => handleSectionClick(7)}><h3>Baby's & Toys</h3></div>
-                <div className={classes['box']} onClick={() => handleSectionClick(8)}><h3>Groceries & Pets</h3></div>
-                <div className={classes['box']} onClick={() => handleSectionClick(9)}><h3>Health & Beauty</h3></div>
+                <div className={classes['box']} onClick={() => handleSectionClick()}><h3>Woman's Fashion</h3></div>
+                <div className={classes['box']} onClick={() => handleSectionClick()}><h3>Man's Fashion</h3></div>
+                <div className={classes['box']} onClick={() => handleSectionClick()}><h3>Electronics</h3></div>
+                <div className={classes['box']} onClick={() => handleSectionClick()}><h3>Home & Lifestyle</h3></div>
+                <div className={classes['box']} onClick={() => handleSectionClick()}><h3>Medicine</h3></div>
+                <div className={classes['box']} onClick={() => handleSectionClick()}><h3>Sports & Outdoor</h3></div>
+                <div className={classes['box']} onClick={() => handleSectionClick()}><h3>Baby's & Toys</h3></div>
+                <div className={classes['box']} onClick={() => handleSectionClick()}><h3>Groceries & Pets</h3></div>
+                <div className={classes['box']} onClick={() => handleSectionClick()}><h3>Health & Beauty</h3></div>
             </div>
             <div className={classes['line']}></div>
             <ImageCarousel />
